12625fa1a6f2d774f0972d4f31ad6a5a1f5001af
Maodo Sow <maodosw@gmail.com>
2019-09-18 12:13:40 +0000

    Hacker Rank Nested python list

--- nestedList.py
+++ nestedList.py
@@ -1,11 +1,21 @@
 
 
 if __name__ == '__main__':
-	students = [['Harry', 37.21], ['Berry', 37.21], ['Tina', 37.2], ['Akriti', 41], ['Harsh', 39]]
+	students = []
+	for _ in range(int(input())):
+		name = input()
+		score = float(input())
+		students.append([name,score])
+	# print(students)
+	# students = [['Harry', 37.21], ['Berry', 37.21], ['Tina', 37.2], ['Akriti', 41], ['Harsh', 39]]
 	array_of_grades = []
 	for student in students :
-		print(student)
 		array_of_grades.append(student[1])
-	print(array_of_grades)
+	# print(min(array_of_grades))
 	min_arr = min([i for i in array_of_grades if i!=min(array_of_grades)])
-	print(min_arr)
+	# print(min_arr)
+	lowest_grades = [students[i][0] for i in range(len(students)) if students[i][1]==min_arr]
+	lowest_grades.sort()
+	for lowest_grade in lowest_grades :
+		print(lowest_grade)
+			
