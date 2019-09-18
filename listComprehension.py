eaeed07eddd5c1e56af26f38b382cb4ce659f9a7
Maodo Sow <maodosw@gmail.com>
2019-09-18 15:23:27 +0000

    Hacker Rank python practice Nested List

--- listComprehension.py
+++ listComprehension.py
@@ -8,13 +8,8 @@ i+j+k != n
 
 if __name__ == '__main__':
 	
-	# x = int(input())
-	# y = int(input())
-	# z = int(input())
-	# n = int(input())
-	x = 1
-	y = 1
-	z = 1
-	n = 2
-
+	x = int(input())
+	y = int(input())
+	z = int(input())
+	n = int(input())
 	print([[i,j,k] for i in range(x+1) for j in range(y+1) for k in range(z+1) if i+j+k!=n])
