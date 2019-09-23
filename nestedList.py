3324a671cc3bdd9203e8593ecaf4a6df5805e50d
Maodo Sow <maodosw@gmail.com>
2019-09-23 13:11:58 +0000

    Nested List Hacker Rank python challenge coding

--- nestedList.py
+++ nestedList.py
@@ -1,13 +1,13 @@
 
 
 if __name__ == '__main__':
-	students = []
-	for _ in range(int(input())):
-		name = input()
-		score = float(input())
-		students.append([name,score])
+	# students = []
+	# for _ in range(int(input())):
+	# 	name = input()
+	# 	score = float(input())
+	# 	students.append([name,score])
 	# print(students)
-	# students = [['Harry', 37.21], ['Berry', 37.21], ['Tina', 37.2], ['Akriti', 41], ['Harsh', 39]]
+	students = [['Harry', 37.21], ['Berry', 37.21], ['Tina', 37.2], ['Akriti', 41], ['Harsh', 39]]
 	array_of_grades = []
 	for student in students :
 		array_of_grades.append(student[1])
